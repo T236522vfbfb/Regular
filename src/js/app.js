189b4bfe52073688aa5f5b231efb7cf9f92bd063
Name Surname <email@email.com>
2024-03-11 20:31:41 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+// TODO: write your code here
+import Bowman from './Bowman';
+import Swordsman from './Swordsman';
+import Magician from './Magician';
+import Daemon from './Daemon';
+import Undead from './Undead';
+import Zombie from './Zombie';
+
+const characterName = new Swordsman("Name Surname")
